4329cf4ecb4acd30e263b223215385ec9be2fadd
Nikita Bochkov <nikiljgryt@gmail.com>
2025-02-13 12:05:45 +0300

    toolBar,explorer

--- src/main/java/org/example/ui/MainUi.java
+++ src/main/java/org/example/ui/MainUi.java
@@ -1,17 +1,24 @@
 package org.example.ui;
 
+import org.example.ApiBack;
+
 import javax.swing.*;
-import javax.swing.border.Border;
-import javax.swing.border.LineBorder;
+import javax.swing.filechooser.FileNameExtensionFilter;
 import java.awt.*;
-import java.util.Optional;
 
-public class MainUi {
-    private static final int WIDTH_LESS = 500;
-    private static final int HEIGHT_LESS = 300;
+public final class MainUi {
+    private static final int WIDTH_LESS;
+    private static final int HEIGHT_LESS;
 
+    static {
+        WIDTH_LESS = 500;
+        HEIGHT_LESS = 300;
+    }
 
-    public static void init() {
+    private static ApiBack api;
+
+    public static void init(ApiBack apiBack) {
+        api = apiBack;
         JFrame frame = new JFrame("Dictionary");
         Dimension winSize = Toolkit.getDefaultToolkit().getScreenSize();
         frame.setSize(winSize.width - WIDTH_LESS, winSize.height - HEIGHT_LESS);
@@ -21,12 +28,26 @@ public class MainUi {
         frame.setVisible(true);
     }
 
+    //when define
     private static void toolBar(JFrame frame) {
         JPanel panel = new JPanel();
         panel.setLayout(new FlowLayout(FlowLayout.LEFT));
-        panel.add(ComponentsUI.btnToolBar());
+        panel.add(ComponentsUI.btnToolBar(
+                MainUi::openExplorer, "Загрузить"
+        ));
         panel.setBackground(Color.WHITE);
         frame.add(panel, BorderLayout.NORTH);
+    }
+
+    private static void openExplorer() {
+        JFileChooser fileChooser = new JFileChooser();
+        fileChooser.setDialogTitle("Выберите файлы");
+        fileChooser.setMultiSelectionEnabled(true);
+        fileChooser.setAcceptAllFileFilterUsed(false);
+        fileChooser.setFileFilter(new FileNameExtensionFilter("Text files (*.txt)", "txt"));
+        if (fileChooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
+            api.parseFiles(fileChooser.getSelectedFiles());
+        }
     }
 
 
