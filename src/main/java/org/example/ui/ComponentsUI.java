11123b6c60f69a45e322a59e0a65d1944472e826
Nikita Bochkov <nikiljgryt@gmail.com>
2025-02-13 10:15:52 +0300

    add event-click

--- src/main/java/org/example/ui/ComponentsUI.java
+++ src/main/java/org/example/ui/ComponentsUI.java
@@ -6,16 +6,23 @@ import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
+interface EventClick {
+    void execute();
+}
+
 public final class ComponentsUI {
 
-    public static Component btnToolBar() {
+    public static Component btnToolBar(EventClick ecl, String text) {
         JButton btn = new JButton();
-        btn.setText("Загрузить");
+        btn.setText(text);
 
         btn.setBackground(Color.LIGHT_GRAY);
 
         btn.setFocusPainted(false);
         btn.setBorder(new EmptyBorder(5, 10, 5, 10));
+        btn.addActionListener(e -> {
+            ecl.execute();
+        });
         btn.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseEntered(MouseEvent e) {
