daec74346412cf9d35ea1c3280a8fd0f42d6462e
Nikita Bochkov <nikiljgryt@gmail.com>
2025-02-18 21:59:12 +0300

    private constructor

--- src/main/java/org/example/DataBaseConnect.java
+++ src/main/java/org/example/DataBaseConnect.java
@@ -5,22 +5,29 @@ import java.sql.DriverManager;
 
 public class DataBaseConnect {
 
-    static Connection Db;
+    private static Connection Db;
 
-    static void connect() {
-        String url = "jdbc:postgresql://localhost:5432/database-dict";
-        String username = "postgres";
-        String password = "12345678";
-
-        try {
-            Class.forName("org.postgresql.Driver");
-            Db = DriverManager.getConnection(
-                    url, username, password);
-            System.out.println("Database connect");
-        } catch (Exception ex) {
-            System.err.println("Ошибка: " + ex);
-            System.exit(1);
-        }
+    private DataBaseConnect() {
 
     }
+
+    public static Connection getDb() {
+        if (Db == null) {
+            String url = "jdbc:postgresql://localhost:5432/database-dict";
+            String username = "postgres";
+            String password = "12345678";
+
+            try {
+                Class.forName("org.postgresql.Driver");
+                Db = DriverManager.getConnection(
+                        url, username, password);
+                System.out.println("Database connect");
+            } catch (Exception ex) {
+                System.err.println("Ошибка: " + ex);
+                System.exit(1);
+            }
+        }
+        return Db;
+    }
+
 }
